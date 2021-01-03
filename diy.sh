cf86fd90f26edc01e44a1992facb87367fdc08c3
muge1976 <69430387+muge1976@users.noreply.github.com>
2021-01-03 17:44:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.10/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Define My Package
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
